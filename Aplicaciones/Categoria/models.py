949e1dfc5b237ff800c20d27b8640a3d28bfe72e
stalin <stalinjaime@gmail.com>
2025-06-09 15:41:01 -0500

    Se agrega categoria

--- Aplicaciones/Categoria/models.py
+++ Aplicaciones/Categoria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def _str_(self):
+        fila = "{0}: {1}"
+        return fila.format(self.id, self.nombre)
