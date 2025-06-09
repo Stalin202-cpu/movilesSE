c57f1618a0945ebe0bcfb88eef8a5129110e513d
stalin <stalinjaime@gmail.com>
2025-06-09 09:08:13 -0500

    Se agregra el modelo de Aplicacion

--- Aplicaciones/Aplicacion/models.py
+++ Aplicaciones/Aplicacion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Aplicacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField()
+    version = models.CharField(max_length=100)
+    fecha = models.DateField()
+
+    def __str__(self): 
+        fila = "{0}:  {1}  {2}   {3} "
+        return fila.format(self.id, self.nombre, self.descripcion, self.version, self.fecha)
