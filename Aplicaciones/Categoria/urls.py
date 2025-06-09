02fc74ad6654d149888bf90b107a346449574bd2
stalin <stalinjaime@gmail.com>
2025-06-09 16:05:05 -0500

    Agregue categorias

---
+++ Aplicaciones/Categoria/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('inicio2',views.inicio2,name='inicio2'),  # este name debe coincidir con tu plantilla
+    path('nuevaCategoria', views.nuevaCategoria),
+    path('guardarCategoria', views.guardarCategoria),
+    path('eliminarCategoria/<id>', views.eliminarCategoria),
+    path('editarCategoria/<id>', views.editarCategoria),
+    path('procesarEdicionCategoria/<id>', views.procesarEdicionCategoria),
+]
--- Aplicaciones/Categoria/views.py
+++ Aplicaciones/Categoria/views.py
@@ -12,18 +12,23 @@ def nuevaCategoria(request):
     return render(request, "nuevaCategoria.html")
 
 # Vista para guardar una nueva categoría
-def guardarCategoria(request):
+def guardarCategoria(request): 
     nombre = request.POST["nombre"]
+
     Categoria.objects.create(nombre=nombre)
+
     messages.success(request, "Categoría creada exitosamente")
-    return redirect('/inicio2/')
+    return redirect('inicio2')
 
 # Vista para eliminar una categoría
 def eliminarCategoria(request, id):
+
     categoriaEliminar = Categoria.objects.get(id=id)
+
     categoriaEliminar.delete()
+
     messages.success(request, "Categoría eliminada exitosamente")
-    return redirect('/inicio2/')
+    return redirect('inicio2')
 
 # Vista para mostrar el formulario de edición
 def editarCategoria(request, id):
@@ -34,6 +39,8 @@ def editarCategoria(request, id):
 def procesarEdicionCategoria(request, id):
     categoria = Categoria.objects.get(id=id)
     categoria.nombre = request.POST["nombre"]
+
     categoria.save()
+    
     messages.success(request, "Categoría actualizada exitosamente")
-    return redirect('/inicio2/')
+    return redirect('inicio2')
